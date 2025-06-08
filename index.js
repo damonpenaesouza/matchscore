55ef7639f2f4874704527f0eb98157b175d406a7
damonpenaesouza <damonpenaesouza@gmail.com>
2025-06-07 22:52:55 -0300

    matchscore

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+let placarc = document.getElementById("casa");
+let placarv = document.getElementById("visita");
+
+function trespc() {
+    placarc.textContent = parseInt(placarc.textContent) + 3;
+}
+
+function doispc() {
+    placarc.textContent = parseInt(placarc.textContent) + 2;
+}
+
+function umpc() {
+    placarc.textContent = parseInt(placarc.textContent) + 1;
+}
+
+function trespv() {
+    placarv.textContent = parseInt(placarv.textContent) + 3;
+}
+
+function doispv() {
+    placarv.textContent = parseInt(placarv.textContent) + 2;
+}
+
+function umpv() {
+    placarv.textContent = parseInt(placarv.textContent) + 1;
+}
+
